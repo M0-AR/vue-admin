ebb615c66cfee4d56931246802145e1adb75050e
Mohamad Ashamr <55653276+M0-AR@users.noreply.github.com>
2022-03-26 14:32:06 +0100

    Display Chart graph.

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,9 +1,49 @@
 <template>
-  Dashboard
+  <h2>Daily Sales</h2>
+
+  <div id="chart"></div>
 </template>
 
-<script>
+<script lang="ts">
+import {onMounted} from "vue";
+import axios from "axios";
+import * as c3 from 'c3';
+
 export default {
-  name: "Dashboard"
+  name: "Dashboard",
+  setup() {
+    onMounted(async () => {
+      var chart = c3.generate({
+        bindto: '#chart',
+        data: {
+          x: 'x',
+          columns: [
+            ['x'],
+            ['Sales']
+          ],
+          types: {
+            Sales: 'bar'
+          }
+        },
+        axis: {
+          x: {
+            type: 'timeseries',
+            tick: {
+              format: '%Y-%m-%d'
+            }
+          }
+        }
+      });
+
+      const {data} = await axios.get('chart');
+
+      chart.load({
+        columns: [
+            ['x', ...data.map((r: any) => r.date)],
+            ['Sales', ...data.map((r: any) => r.sum)],
+        ]
+      });
+    });
+  }
 }
 </script>
